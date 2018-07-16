8c55f1c2c69533fd94a981b3dbd8c6c97dea430b
osoyliz <lizosoy18@gmail.com>
2018-07-16 18:31:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,10 @@
 # dog.rb
+class Dog
+  attr_accessor :name_1, :name_2, :name_3
+  
+  def initialize(name_1, name_2, name_3)
+    @name_1 = name_1
+    @name_2 = name_2
+    @name_3 = name_3
+  end
+end
